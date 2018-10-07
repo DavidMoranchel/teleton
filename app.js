a77a080f8fa955b848eaeae181734a910b31e890
David Moranchel <david.arcshtag@gmail.com>
2018-10-06 22:51:06 -0500

    test

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const r = require('rethinkdb');
 
-http.createServer( (req, res) => {
-  res.end('Work it!')
-}).listen(8081);
+r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    // connection = conn;
+    console.log(conn);
+})
+
+
+// http.createServer( (req, res) => {
+//   res.end('Work it!')
+// }).listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+// console.log('Server running at http://127.0.0.1:8081/');
